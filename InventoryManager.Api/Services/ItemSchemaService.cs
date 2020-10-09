73085271d7798295f1c10425eef17dec454ad11d
Shahab <shahabdsh@gmail.com>
2020-10-08 22:05:21 -0400

    Adding generic repository service to the backend

--- InventoryManager.Api/Services/ItemSchemaService.cs
+++ InventoryManager.Api/Services/ItemSchemaService.cs
@@ -1,48 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using InventoryManager.Api.Models;
+﻿using InventoryManager.Api.Models;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 
 namespace InventoryManager.Api.Services
 {
-    public class ItemSchemaService : IItemSchemaService
+    public class ItemSchemaService : GenericRepositoryService<ItemSchema>, IItemSchemaService
     {
-        private readonly IMongoCollection<ItemSchema> _itemSchemas;
+        protected override string ItemCollectionName => "ItemSchemas";
 
-        public ItemSchemaService(IOptions<InventoryDatabaseSettings> settings)
+        public ItemSchemaService(IOptions<InventoryDatabaseSettings> settings) : base(settings)
         {
-            var client = new MongoClient(settings.Value.ConnectionString);
-            var database = client.GetDatabase(settings.Value.DatabaseName);
-
-            _itemSchemas = database.GetCollection<ItemSchema>(settings.Value.ItemSchemaCollectionName);
         }
-
-        public List<ItemSchema> Get() =>
-            _itemSchemas.Find(itemSchema => true).ToList();
-
-        public ItemSchema Get(string id) =>
-            _itemSchemas.Find<ItemSchema>(itemSchema => itemSchema.Id == id).FirstOrDefault();
-
-        public ItemSchema Create(ItemSchema itemSchema)
-        {
-            _itemSchemas.InsertOne(itemSchema);
-            return itemSchema;
-        }
-
-        public void Update(string id, ItemSchema itemSchemaIn)
-        {
-            _itemSchemas.ReplaceOne(itemSchema => itemSchema.Id == id, itemSchemaIn);
-        }
-            
-
-        public void Remove(ItemSchema itemSchemaIn) =>
-            _itemSchemas.DeleteOne(itemSchema => itemSchema.Id == itemSchemaIn.Id);
-
-        public void Remove(string id) => 
-            _itemSchemas.DeleteOne(itemSchema => itemSchema.Id == id);
     }
 }
--- InventoryManager.Api/Services/ItemService.cs
+++ InventoryManager.Api/Services/ItemService.cs
@@ -1,48 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using InventoryManager.Api.Models;
+﻿using InventoryManager.Api.Models;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 
 namespace InventoryManager.Api.Services
 {
-    public class ItemService : IItemService
+    public class ItemService : GenericRepositoryService<Item>, IItemService
     {
-        private readonly IMongoCollection<Item> _items;
-
-        public ItemService(IOptions<InventoryDatabaseSettings> settings)
+        protected override string ItemCollectionName => "Items";
+        
+        public ItemService(IOptions<InventoryDatabaseSettings> settings) : base(settings)
         {
-            var client = new MongoClient(settings.Value.ConnectionString);
-            var database = client.GetDatabase(settings.Value.DatabaseName);
-
-            _items = database.GetCollection<Item>(settings.Value.ItemCollectionName);
         }
-
-        public List<Item> Get() =>
-            _items.Find(item => true).ToList();
-
-        public Item Get(string id) =>
-            _items.Find<Item>(item => item.Id == id).FirstOrDefault();
-
-        public Item Create(Item item)
-        {
-            _items.InsertOne(item);
-            return item;
-        }
-
-        public void Update(string id, Item itemIn)
-        {
-            _items.ReplaceOne(item => item.Id == id, itemIn);
-        }
-            
-
-        public void Remove(Item itemIn) =>
-            _items.DeleteOne(item => item.Id == itemIn.Id);
-
-        public void Remove(string id) => 
-            _items.DeleteOne(item => item.Id == id);
     }
 }
