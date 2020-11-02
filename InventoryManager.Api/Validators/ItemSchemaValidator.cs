66aad47bf4d3eb7250cc0682c490be7db606b12e
Shahab <shahabdsh@gmail.com>
2020-11-01 23:50:24 -0500

    Forcing item schema name to be unique. Updating Ideas.txt

--- InventoryManager.Api/Validators/ItemSchemaValidator.cs
+++ InventoryManager.Api/Validators/ItemSchemaValidator.cs
@@ -1,13 +1,18 @@
 ﻿using System.Linq;
 using FluentValidation;
 using InventoryManager.Api.Models;
+using InventoryManager.Api.Services;
 
 namespace InventoryManager.Api.Validators
 {
     public class ItemSchemaValidator : AbstractValidator<ItemSchema>
     {
-        public ItemSchemaValidator() {
-            RuleFor(x => x.Name).NotEmpty();
+        public ItemSchemaValidator(IItemSchemaService itemSchemaService) {
+            RuleFor(x => x.Name).NotEmpty()
+                .Must((schema,name) =>
+                    !itemSchemaService.Queryable()
+                        .Any(s => s.Id != schema.Id && s.Name == name))
+                .WithMessage(schema => $"Item schema name must be unique.");
             RuleFor(x => x.Properties).Must(list =>
                     list.Select(x => x.Name).Distinct().Count() == list.Count)
                 .WithMessage(item => $"Item schema can not contain duplicate fields");
